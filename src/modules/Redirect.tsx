0cc4865d5200d55be6764cf736a14784d305392d
Ayano <s.correa.wilches@gmail.com>
2023-05-30 17:26:02 -0500

    Add temporary redirection to GitHub

---
+++ src/modules/Redirect.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from "react";
+
+export default function Redirect() {
+  const anchorRef = useRef(null);
+
+  useEffect(() => {
+    anchorRef.current.click();
+  });
+
+  return <a href="https://github.com/AyanoNK" ref={anchorRef}></a>;
+}
