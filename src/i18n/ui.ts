547fa70a8c77fc42ee55189c0d3a03e079c38db5
AyanoNK <s.correa.wilches@gmail.com>
2023-12-25 14:45:28 -0500

    add first translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,69 @@
+export const languages = {
+  en: "English",
+  es: "Spanish",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "header.home": "Home",
+    "header.about": "About",
+    "header.work": "Work",
+    "header.projects": "Projects",
+    "index.title": "Full Stack Software Engineer",
+    "index.expertise.title": "Expertise",
+    "index.expertise.frontend.title": "Frontend Development",
+    "index.expertise.frontend.description":
+      "Experienced in React, React Native & HAML, using Redux & React Query, etc., to build responsive, performant, and accessible applications.",
+    "index.expertise.backend.title": "Backend Development",
+    "index.expertise.backend.description":
+      "Experienced in backend development with both Django & Ruby on Rails, exposing endpoints in both REST and GraphQL.",
+    "index.expertise.database.title": "Database Development",
+    "index.expertise.database.description":
+      "Worked with relational databases like PostgreSQL and MySQL, and new ─ cutting edge NoSQL like Firebase.",
+    "index.expertise.devops.title": "DevOps",
+    "index.expertise.devops.description":
+      "Deployed both professional and personal projects using AWS S3, Google Cloud, Heroku, and Vercel.",
+  },
+  es: {
+    "header.home": "Inicio",
+    "header.about": "Sobre mí",
+    "header.work": "Experiencia",
+    "header.projects": "Proyectos",
+    "index.title": "Ingeniero de Software Full Stack",
+    "index.expertise.title": "Pericia",
+    "index.expertise.frontend.title": "Desarrollo Frontend",
+    "index.expertise.frontend.description":
+      "Experiencia en React, React Native & HAML, usando Redux & React Query, etc., para construir aplicaciones responsivas, de alto rendimiento y accesibles.",
+    "index.expertise.backend.title": "Desarrollo Backend",
+    "index.expertise.backend.description":
+      "Experiencia en desarrollo backend con Django & Ruby on Rails, exponiendo endpoints en REST y GraphQL.",
+    "index.expertise.database.title": "Desarrollo de Base de Datos",
+    "index.expertise.database.description":
+      "Trabajado con bases de datos relacionales como PostgreSQL y MySQL, y nuevas ─ de vanguardia NoSQL como Firebase.",
+    "index.expertise.devops.title": "DevOps",
+    "index.expertise.devops.description":
+      "Desplegado proyectos profesionales y personales usando AWS S3, Google Cloud, Heroku, y Vercel.",
+  },
+  ja: {
+    "header.home": "ホーム",
+    "header.about": "私について",
+    "header.work": "経験",
+    "header.projects": "プロジェクト",
+    "index.title": "フルスタックソフトウェアエンジニア",
+    "index.expertise.title": "専門知識",
+    "index.expertise.frontend.title": "フロントエンド開発",
+    "index.expertise.frontend.description":
+      "React、React Native & HAMLの経験があり、Redux & React Queryなどを使用して、応答性、パフォーマンス、アクセシビリティの高いアプリケーションを構築しています。",
+    "index.expertise.backend.title": "バックエンド開発",
+    "index.expertise.backend.description":
+      "Django & Ruby on Railsの両方でバックエンド開発の経験があり、RESTとGraphQLの両方でエンドポイントを公開しています。",
+    "index.expertise.database.title": "データベース開発",
+    "index.expertise.database.description":
+      "PostgreSQLやMySQLなどのリレーショナルデータベース、Firebaseなどの新しい最先端のNoSQLなどを使用しています。",
+    "index.expertise.devops.title": "DevOps",
+    "index.expertise.devops.description":
+      "AWS S3、Google Cloud、Heroku、Vercelなどを使用して、プロジェクトをデプロイしました。",
+  },
+} as const;
