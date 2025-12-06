204d865b85c0929121c288196950ac4b12892bbc
Ayano <s.correa.wilches@gmail.com>
2025-12-06 17:27:11 -0500

    update project astro config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,8 @@
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   tailwindConfig: "./tailwind.config.cjs",
+  bracketSameLine: true,
+  htmlWhitespaceSensitivity: "ignore",
   overrides: [
     {
       files: "*.astro",
